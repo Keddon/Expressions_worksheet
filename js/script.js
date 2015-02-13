e6e3be4446e45548209fb975374f0d67c532e0ca
Keddon Campbell <mrkfed101@gmail.com>
2015-02-12 19:39:19 -0500

    Worksheet
    Added the third question and made some changes to the values of the
    second question

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ var sparkyAge; //this is Sparky's age in human age
 var growthRate = 7; //this is the rate difference which is 7 times faster
 var dogAge; //this is Sparky's age in dog years
 
-console.log("lets assume that Skarky is 6 years old in human years");
+console.log("lets assume that Skarky is 6 years old in human years.");
 sparkyAge = 6;
 dogAge = sparkyAge * growthRate; //calculates sparky's age.
 console.log(dogAge);
 
-console.log("Sparky is years"+" "+sparkyAge +" "+ "in human years, but is"+" "+dogAge +" "+ "in dog years");
+console.log("Sparky is years"+" "+sparkyAge +" "+ "in human years, but is"+" "+dogAge +" "+ "in dog years.");
 
 
 
@@ -26,18 +26,22 @@ console.log("Sparky is years"+" "+sparkyAge +" "+ "in human years, but is"+" "+d
     //Slice Of Pie part 1/
 
 
-var pizzaSlice = 5;
-var partyGoers = 30;
-var pizzaOrder = 3;
+var pizzaSlice = 6;
+var partyGoers = 20;
+var pizzaOrder = 7;
 
 var totalSlice = pizzaSlice * pizzaOrder;
-var eachPerson = totalSlice / partyGoers
+var eachPerson = totalSlice / partyGoers;
 console.log(totalSlice);
 console.log(eachPerson);
 
-console.log("Each person ate"+" "+eachPerson+" "+"number of slices");
+console.log("Each person ate"+" "+eachPerson+" "+"number of slices.");
 
 
 
     //Slice Of Pie part 2/
 
+
+var sparkySlice = totalSlice % partyGoers;
+console.log(sparkySlice);
+console.log("Spartk ate"+" "+ sparkySlice+ " "+ "that night.");
