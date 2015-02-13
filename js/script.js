50d1b4c28ebf5d4eca589751dede2f0e13f702b8
Keddon Campbell <mrkfed101@gmail.com>
2015-02-12 19:24:14 -0500

    Worksheet
    Added the second Problem to the script file. It works perfectly fine.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
  * Created by keddoncampbell on 2/12/15 Expression Worksheet.
  */
 
+
+
+    //Sparky The Dog/
+
 var sparkyAge; //this is Sparky's age in human age
 var growthRate = 7; //this is the rate difference which is 7 times faster
 var dogAge; //this is Sparky's age in dog years
@@ -12,3 +16,28 @@ dogAge = sparkyAge * growthRate; //calculates sparky's age.
 console.log(dogAge);
 
 console.log("Sparky is years"+" "+sparkyAge +" "+ "in human years, but is"+" "+dogAge +" "+ "in dog years");
+
+
+
+
+
+
+
+    //Slice Of Pie part 1/
+
+
+var pizzaSlice = 5;
+var partyGoers = 30;
+var pizzaOrder = 3;
+
+var totalSlice = pizzaSlice * pizzaOrder;
+var eachPerson = totalSlice / partyGoers
+console.log(totalSlice);
+console.log(eachPerson);
+
+console.log("Each person ate"+" "+eachPerson+" "+"number of slices");
+
+
+
+    //Slice Of Pie part 2/
+
